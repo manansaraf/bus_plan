1dca4c83c053c3c1296ee76150b0f617a1d2ccbc
Dylan Rebelak <drebelak@gmail.com>
2015-02-17 20:04:32 -0600

    added simple search stops page and adjusted some general formatting. changed style to device default to prevent crashing on non-emulator devices.

--- BusPathPrototype/app/src/main/java/com/davidtoh/helloworld/FavoriteStops.java
+++ BusPathPrototype/app/src/main/java/com/davidtoh/helloworld/FavoriteStops.java
@@ -5,7 +5,6 @@ package com.davidtoh.helloworld;
  */
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -27,16 +26,17 @@ public class FavoriteStops extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+		// Handle presses on the action bar items
+		switch (item.getItemId()) {
+			case R.id.action_settings:
+				//openSettings();
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+	}
 }
--- BusPathPrototype/app/src/main/java/com/davidtoh/helloworld/MainActivity.java
+++ BusPathPrototype/app/src/main/java/com/davidtoh/helloworld/MainActivity.java
@@ -2,7 +2,6 @@ package com.davidtoh.helloworld;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.app.ActionBar;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -28,18 +27,24 @@ public class MainActivity extends Activity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+		// Handle presses on the action bar items
+		switch (item.getItemId()) {
+			case R.id.action_settings:
+				//openSettings();
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+	}
 
     public void toFavoriteStops(View view) {
         Intent changeToFavorites = new Intent(view.getContext(), FavoriteStops.class);
         startActivityForResult(changeToFavorites, 0);
     }
+
+	public void toSearchStops(View view) {
+		Intent changeToSearch = new Intent(view.getContext(), SearchStops.class);
+		startActivityForResult(changeToSearch, 0);
+	}
 }
