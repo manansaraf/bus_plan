cd72281b39de9c9ae4243d32a1b632e5a5c393c9
ZEFFER14 <boomersrs@gmail.com>
2015-05-02 21:33:17 -0500

    Reciprocated comments added in master branch

--- BusPathPrototype/app/src/main/java/com/davidtoh/helloworld/widgets/BusStopWidgetProvider.java
+++ BusPathPrototype/app/src/main/java/com/davidtoh/helloworld/widgets/BusStopWidgetProvider.java
@@ -14,10 +14,17 @@ import com.davidtoh.helloworld.R;
 import com.davidtoh.helloworld.core_activities.BusStopStatisticsActivity;
 
 /**
- * Created by davidtoh on 4/7/15.
+ * Created by davidtoh on 4/7/15. This class is responsible for managing and updating widgets. 
  */
 public class BusStopWidgetProvider extends AppWidgetProvider {
 
+        /**
+         * Gets called when the widget needs to be updated. Behavior is different depending on if the widget
+         * has already been configured or needs to be configured (i.e., it's already been bound to a stop and color).
+         * @param context Widget Context
+         * @param appWidgetManager updates and manages Widgets
+         * @param appWidgetIds list of app widget ID's
+         */
         public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
             final int N = appWidgetIds.length;
 
